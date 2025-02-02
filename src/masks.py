2c4bbc0658f453f376ad3432ece98a100c58f7fb
pavelk <kadkinpasa@gmail.com>
2025-02-02 15:39:46 +0300

    hw2 commit 5

--- src/masks.py
+++ src/masks.py
@@ -1,12 +1,9 @@
-def get_mask_card_number(card_number: int) -> str:
+def get_mask_card_number(card_number: str) -> str:
     """Функция, которая принимает на вход номер карты и возвращает её маску"""
-    card_number = str(card_number)
-    new_mask_card = card_number[:4] + ' ' + card_number[6:8] + '** ****' + ' ' + card_number[-4:]
+    new_mask_card = card_number[:4] + " " + card_number[6:8] + "** ****" + " " + card_number[-4:]
     return new_mask_card
 
 
-def get_mask_account(card_number: int) -> str:
+def get_mask_account(card_number: str) -> str:
     """Функция, которая принимает на вход номер счёта и возвращает его маску"""
-    card_number = str(card_number)
-    return '**' + card_number[-4:]
-
+    return "**" + card_number[-4:]
--- src/widget.py
+++ src/widget.py
@@ -1,21 +1,21 @@
-from masks import get_mask_card_number, get_mask_account
+from masks import get_mask_account, get_mask_card_number
 
 
 def mask_account_card(card_info: str) -> str:
     """Функция, которая обрабатывает информацию как о картах, так и о счетах"""
     parts = card_info.split()
 
-    if 'Счет' in parts:
+    if "Счет" in parts:
         account_number = parts[-1]
         masked_number = get_mask_account(account_number)
-        print(f"{' '.join(parts[:-1])} {masked_number}")
+        return f"{' '.join(parts[:-1])} {masked_number}"
     else:
-        card_name = ' '.join(parts[:-1])
+        card_name = " ".join(parts[:-1])
         card_number = parts[-1]
         masked_number = get_mask_card_number(card_number)
-        print(f'{card_name} {masked_number}')
+        return f"{card_name} {masked_number}"
 
 
 def get_date(date: str) -> str:
-    new_date = date.split('-')
-    return f'{new_date[2][:2]}.{new_date[1]}.{new_date[0]}'
+    new_date = date.split("-")
+    return f"{new_date[2][:2]}.{new_date[1]}.{new_date[0]}"
