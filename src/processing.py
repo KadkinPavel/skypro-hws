66ea870a1b29a248e4b534b9b002ab1b4a41b401
pavelk <kadkinpasa@gmail.com>
2025-02-12 18:24:03 +0300

    hw done 2.0

--- src/processing.py
+++ src/processing.py
@@ -9,5 +9,5 @@ def filter_by_state(info: list[dict], state: str = 'EXECUTED') -> list[dict]:
 
 
 def sort_by_date(info: list[dict], reverse=False) -> list[dict]:
-     Функция должна возвращать новый список, отсортированный по дате"""
-    return sorted(info, key=lambda x: x['date'], reverse=reverse)
+     """Функция должна возвращать новый список, отсортированный по дате"""
+     return sorted(info, key=lambda x: x['date'], reverse=reverse)
