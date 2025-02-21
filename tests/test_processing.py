6e9a97397e1bb70bf3e7152baffbf69392758e47
pavelk <kadkinpasa@gmail.com>
2025-02-21 21:16:33 +0300

    hw done

--- tests/test_processing.py
+++ tests/test_processing.py
@@ -1,3 +1,5 @@
+import pytest
+
 from src.processing import filter_by_state, sort_by_date
 
 test_data = [{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
@@ -10,7 +12,7 @@ test_data = [{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.
 def test_filter_by_state():
     """Тестирование функции filter_by_state"""
 
-    #Тестирование фильтрации по статусу "EXECUTED"
+    # Тестирование фильтрации по статусу "EXECUTED"
     result = filter_by_state(test_data, "EXECUTED")
     expected = [
         {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
@@ -18,14 +20,13 @@ def test_filter_by_state():
     ]
     assert result == expected
 
-    #Тестирование фильтрации по статусу "CANCELED"
+    # Тестирование фильтрации по статусу "CANCELED"
     result = filter_by_state(test_data, "CANCELED")
     expected = [
         {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
         {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
     ]
     assert result == expected
-
 
 
 test_data_2 = [
@@ -39,7 +40,7 @@ test_data_2 = [
 def test_sort_by_date():
     """Тестирование функции sort_by_date"""
 
-    #Тестирование сортировки по дате в порядке возрастания
+    # Тестирование сортировки по дате в порядке возрастания
     result = sort_by_date(test_data)
     expected = [
         {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
@@ -49,7 +50,7 @@ def test_sort_by_date():
     ]
     assert result == expected
 
-    #Тестирование сортировки по дате в порядке убывания
+    # Тестирование сортировки по дате в порядке убывания
     result = sort_by_date(test_data, reverse=True)
     expected = [
         {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
@@ -59,7 +60,6 @@ def test_sort_by_date():
     ]
     assert result == expected
 
-    #Тестирование сортировки пустого списка
+    # Тестирование сортировки пустого списка
     result = sort_by_date([])
     assert result == []
-
